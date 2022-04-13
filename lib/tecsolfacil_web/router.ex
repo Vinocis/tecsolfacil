49a0c9856dc83e5990904df1e1038c80adc0be97
Vinocis <vinicius.alexandre68@gmail.com>
2022-04-13 20:17:21 -0300

    adding the routes

--- lib/tecsolfacil_web/router.ex
+++ lib/tecsolfacil_web/router.ex
@@ -5,7 +5,11 @@ defmodule TecsolfacilWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", TecsolfacilWeb do
+  scope "/api", TecsolfacilWeb.Api, as: :api do
     pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+      get "/address/:cep", AddressController, :show
+    end
   end
 end
