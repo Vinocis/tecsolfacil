27995645f6e96eb8fbe5abb1b3caba2db3336fdf
Vinocis <vinicius.alexandre68@gmail.com>
2022-04-14 17:04:08 -0300

    fixing tests

--- test/tecsolfacil_workers/create_csv_from_address_test.exs
+++ test/tecsolfacil_workers/create_csv_from_address_test.exs
@@ -5,14 +5,9 @@ defmodule TecsolfacilWorkers.CreateCsvFromAddressTest do
 
   describe "perform/1" do
     test "success when the user exists" do
-      file_name =
-        NaiveDateTime.local_now()
-        |> NaiveDateTime.to_string()
-        |> String.replace(~r/[^\d]/, "")
-
       assert {:ok, _} =
                perform_job(CreateCsvFromAddress, %{
-                 file_name: file_name
+                 file_name: "file_name"
                })
     end
   end
